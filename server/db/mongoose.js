076115f13f94acb29c45f19e3581545a183af297
Phillip-V-Molodtsov <40450879+Phillip-V-Molodtsov@users.noreply.github.com>
2019-01-01 21:22:55 +0200

    part2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 let mongoDBURI1 = 'mongodb://localhost:27017/MongooseApp';
-let mongoDBURI2 = 'mongodb://philvm:philvm123@ds145194.mlab.com:45194/fishnik';
-let mongodb_uri = process.env.MONGODB_URI?mongoDBURI2:mongoDBURI1;
+let mongoDBURI2 = 'mongodb://phil:phil123@ds145194.mlab.com:45194/fishnik';
 mongoose.connect(mongoDBURI2, { useNewUrlParser: true });
 
 module.exports ={mongoose};

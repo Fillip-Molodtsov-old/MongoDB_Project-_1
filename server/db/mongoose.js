12135547d4cf2096271655b8f094f2d9f3793060
Phillip-V-Molodtsov <40450879+Phillip-V-Molodtsov@users.noreply.github.com>
2019-01-01 21:35:52 +0200

    deploying to heroku p2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 let mongoDBURI1 = 'mongodb://localhost:27017/MongooseApp';
 let mongoDBURI2 = 'mongodb://phil:phil123@ds145194.mlab.com:45194/fishnik';
-mongoose.connect(mongoDBURI2, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI?mongoDBURI2:mongoDBURI1, { useNewUrlParser: true });
 
 module.exports ={mongoose};

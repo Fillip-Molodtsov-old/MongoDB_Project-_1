5fb3b011ab2b4c32a3f8278b24e5a6762f34fc2e
Phillip-V-Molodtsov <40450879+Phillip-V-Molodtsov@users.noreply.github.com>
2019-01-01 21:38:45 +0200

    deploying to heroku p4

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 let mongoDBURI1 = 'mongodb://localhost:27017/MongooseApp';
 let mongoDBURI2 = 'mongodb://phil:phil123@ds145194.mlab.com:45194/fishnik';
-mongoose.connect(process.env.MONGODB_PORT?mongoDBURI2:mongoDBURI1, { useNewUrlParser: true });
+//you can find the difference between heroku and local app by the port
+mongoose.connect(process.env.PORT ? mongoDBURI2 : mongoDBURI1, { useNewUrlParser: true });
 
-module.exports ={mongoose};
+module.exports = { mongoose };

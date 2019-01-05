5ac9b11131867dec02f7c80b76a8de8a84f69295
Phillip-V-Molodtsov <40450879+Phillip-V-Molodtsov@users.noreply.github.com>
2019-01-06 01:28:47 +0200

    last touches

--- server/config/config-env.js
+++ server/config/config-env.js
@@ -1,6 +1,5 @@
 let env = process.env.NODE_ENV || 'dev';
 console.log(env);
-//process.env.MongoDB_URI = 'mongodb://phil:phil123@ds145194.mlab.com:45194/fishnik';
 
 if(env=="dev"||env=="test"){
     let config = require('./config-env.json');
